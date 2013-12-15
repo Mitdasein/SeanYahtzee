20e3d0a0984c1b0f0815ab024a3c817fdc291de3
Sean Scott <sean.scott@gmail.com>
2013-12-14 22:23:59 -0800

    added 3-roll function

--- SeanYahtzee.js
+++ SeanYahtzee.js
@@ -1,54 +1,90 @@
-$(document).ready(function(){
-
-  var randomdice1 
-  var randomdice2
-  var randomdice3
-  var randomdice4
-  var randomdice5 
+  var randomdice1;
+  var randomdice2;
+  var randomdice3;
+  var randomdice4;
+  var randomdice5;
 
   var currentDice = [];
   var diceTotal = 0;
 
-  $("button.roll").click(function(){
-   randomdice1 = (Math.round(Math.random()*5)+1);
-   randomdice2 = (Math.round(Math.random()*5)+1);
-   randomdice3 = (Math.round(Math.random()*5)+1);
-   randomdice4 = (Math.round(Math.random()*5)+1);
-   randomdice5 = (Math.round(Math.random()*5)+1);
-   $("button.die1").html(randomdice1);
-   $("button.die2").html(randomdice2);
-   $("button.die3").html(randomdice3);
-   $("button.die4").html(randomdice4);
-   $("button.die5").html(randomdice5);
+  var tries = 0;
 
-  /* put rolled dice into array */
-  currentDice[0]=randomdice1;
-  currentDice[1]=randomdice2;
-  currentDice[2]=randomdice3;
-  currentDice[3]=randomdice4;
-  currentDice[4]=randomdice5;
+  $(document).ready(function()
+    {
+      $("button.roll").click(function()
+        {
+          if ( tries >= 3 ) 
+            {
+              alert("You have used all your rolls for this turn!")
+            } 
+              else 
+            { 
+              randomdice1 = (Math.round(Math.random()*5)+1);
+              randomdice2 = (Math.round(Math.random()*5)+1);
+              randomdice3 = (Math.round(Math.random()*5)+1);
+              randomdice4 = (Math.round(Math.random()*5)+1);
+              randomdice5 = (Math.round(Math.random()*5)+1);
+              $("button.die1").html(randomdice1);
+              $("button.die2").html(randomdice2);
+              $("button.die3").html(randomdice3);
+              $("button.die4").html(randomdice4);
+              $("button.die5").html(randomdice5);
 
- })
+              /* put rolled dice into array */
+              currentDice[0]=randomdice1;
+              currentDice[1]=randomdice2;
+              currentDice[2]=randomdice3;
+              currentDice[3]=randomdice4;
+              currentDice[4]=randomdice5;
+
+              for(var i=0; i<currentDice.length; i++) 
+                {
+                  diceTotal += currentDice[i];
+                }
+        
+              tries=++tries; 
+            }
+        }
+      )
+    }
+  );
+
+  function seeTotal()
+  {
+    if (currentDice.length===0)
+    {
+      alert("You haven't rolled yet!");
+    }
+    else
+    {
+     // for(var i=0; i<currentDice.length; i++) 
+     // {
+     //   diceTotal += currentDice[i];
+     // }
+     alert("Your total is " + diceTotal);
+           // document.write("Your total is " + diceTotal);
+         }
+       };
 
 
-});
+       function score()
+       {  
+        console.log(diceTotal);
+        alert("Your total is " + diceTotal); 
+      };
 
-function score()
- {
-  for(var i=0; i<currentDice.length; i++) {
-   diceTotal += currentDice[i];
-   console.log(diceTotal);
-   // alert("Your total is " + diceTotal); 
-   alert("score!");
- }
-} 
-
-
-function rollAgain()
-{
-  alert("Roll again!");
-}
-
+      function rollAgain()
+      {
+        if (currentDice.length===0)
+        {
+          alert("You haven't rolled yet!");
+        }
+        else
+        {
+          alert("Not yet implemented!");
+        }
+      }
+      ;
 
 
 
